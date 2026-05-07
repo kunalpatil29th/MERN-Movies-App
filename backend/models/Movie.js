8ca9f2034935383e93d7f38b64af28e38dff0997
Kunal Patil <kunalpatil29th@gmail.com>
2026-05-07 21:09:58 +0530

    Update Movie.js

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -1,17 +1,5 @@
 import mongoose from "mongoose";
-const { ObjectId } = mongoose.Schema;
-
-const reviewSchema = mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    rating: { type: Number, required: true },
-    comment: { type: String, required: true },
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "User",
-    },
-  },
+c
   { timestamps: true }
 );
 
