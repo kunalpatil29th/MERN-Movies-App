ef138fc61d0bfb5ada22834a8b23fee8d19c903a
Kunal Patil <kunalpatil29th@gmail.com>
2026-05-16 10:51:48 +0530

    Fix import statement and export Movie model

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -1,22 +1,2 @@
-import mongoose from "mongoose";
-c
-  { timestamps: true }
-);
-
-const movieSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    image: { type: String },
-    year: { type: Number, required: true },
-    genre: { type: ObjectId, ref: "Genre", required: true },
-    detail: { type: String, required: true },
-    cast: [{ type: String }],
-    reviews: [reviewSchema],
-    numReviews: { type: Number, required: true, default: 0 },
-    createdAt: { type: Date, default: Date.now },
-  },
-  { timestamps: true }
-);
-
-const Movie = mongoose.model("Movie", movieSchema);
+e", movieSchema);
 export default Movie;
